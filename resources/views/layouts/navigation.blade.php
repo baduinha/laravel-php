e8cf4cf54e2dbce4305aaa38b69e143ee7f2eff1
Claudio Volz <badunaarea@hotmail.com>
2026-03-24 10:58:41 -0300

    nav responsiva

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -75,8 +75,13 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
+                @auth
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+            @else
+                <div class="font-medium text-base text-gray-800">Visitante</div>
+                <div class="font-medium text-sm text-gray-500">Não autenticado</div>
+            @endauth
             </div>
 
             <div class="mt-3 space-y-1">
