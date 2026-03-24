57799cc4203ac3b5c975f1293c8a5c3a898ad984
Claudio Volz <badunaarea@hotmail.com>
2026-03-24 10:15:39 -0300

    adicionei as importações necessárias nos modelos.

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Post;
+use App\Models\User;
 
 class Comment extends Model
 {
